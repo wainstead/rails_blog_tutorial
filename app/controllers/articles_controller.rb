e382f6d9ce71831ae00438cb52edc2fe7ba5e187
Steve Wainstead <swain@panix.com>
2018-11-11 13:23:58 -0500

    Implemented the CRU operations of CRUD
    And navigation goodness

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,17 +8,35 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     #render plain: params[:article].inspect
     #@article = Article.new(params[:article])
     #@article = Article.new(params.require(:article).permit(:title, :text))
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
   private
   def article_params
     params.require(:article).permit(:title, :text)
